50d65549a8758d3f8d2907785fbd608f06f56013
Malith Dilshan <it21180934@my.sliit.lk>
2025-01-22 13:52:28 +0530

    type error fixed in product creation ✅

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -35,8 +35,8 @@ export async function POST(req: NextRequest) {
 	try {
 		const body = await req.json();
 
-		const {
-            barcode,
+		let {
+			barcode,
 			key,
 			name,
 			stock,
@@ -72,11 +72,25 @@ export async function POST(req: NextRequest) {
 				{ status: 400 }
 			);
 		}
+		//convert stock, volume, default_labeled_price, default_cost to float
+		stock = parseFloat(stock);
+		volume = parseFloat(volume);
+		default_labeled_price = parseFloat(default_labeled_price);
+		default_cost = parseFloat(default_cost);
+		uom = parseFloat(uom);
 
+		//convert barcode, key, name, container_type, uom, flavour, product_image, status to string
+		barcode = barcode.toString();
+		key = key.toString();
+		name = name.toString();
+		container_type = container_type.toString();
+		flavour = flavour.toString();
+		product_image = product_image.toString();
+		status = status.toString();
 		// Create product
 		const product = await prisma.product.create({
 			data: {
-                barcode,
+				barcode,
 				key,
 				name,
 				stock,
@@ -93,12 +107,16 @@ export async function POST(req: NextRequest) {
 
 		return NextResponse.json({ message: "Product created", product });
 	} catch (e: any) {
-        if(e.message.includes("Unique constraint failed on the fields: (`key`)")){
-            return NextResponse.json(
-                { message: "Product key already exists try using different product key", error: e },
-                { status: 400 }
-            );
-        }
+		if (e.message.includes("Unique constraint failed on the fields: (`key`)")) {
+			return NextResponse.json(
+				{
+					message: "Product key already exists try using different product key",
+					error: e,
+				},
+				{ status: 400 }
+			);
+		}
+
 		return NextResponse.json(
 			{ message: "Error creating product", error: e },
 			{ status: 500 }
@@ -107,116 +125,131 @@ export async function POST(req: NextRequest) {
 }
 
 export async function PUT(req: NextRequest) {
-    const userHeader = req.headers.get("user");
-    const productKey = req.nextUrl.searchParams.get("key");
-    const user: UserType | null = userHeader ? JSON.parse(userHeader) : null;
+	const userHeader = req.headers.get("user");
+	const productKey = req.nextUrl.searchParams.get("key");
+	const user: UserType | null = userHeader ? JSON.parse(userHeader) : null;
 
-    if (!user) {
-        return NextResponse.json({ message: "User not found" }, { status: 404 });
-    }
+	if (!user) {
+		return NextResponse.json({ message: "User not found" }, { status: 404 });
+	}
 
-    if (user.role !== "admin") {
-        return NextResponse.json(
-            { message: "Unauthorized: Only admin can update products" },
-            { status: 401 }
-        );
-    }
+	if (user.role !== "admin") {
+		return NextResponse.json(
+			{ message: "Unauthorized: Only admin can update products" },
+			{ status: 401 }
+		);
+	}
 
-    try {
-        const body = await req.json();
+	try {
+		const body = await req.json();
 
-        const {
-            barcode,
-            key,
-            name,
-            stock,
-            container_type,
-            uom,
-            volume,
-            flavour,
-            default_labeled_price,
-            default_cost,
-            product_image,
-            status,
-        } = body;
+		let {
+			barcode,
+			key,
+			name,
+			stock,
+			container_type,
+			uom,
+			volume,
+			flavour,
+			default_labeled_price,
+			default_cost,
+			product_image,
+			status,
+		} = body;
 
-        // Validation
-        if (
-            !key ||
-            !name ||
-            !stock ||
-            !container_type ||
-            !uom ||
-            !volume ||
-            !flavour ||
-            !default_labeled_price ||
-            !default_cost ||
-            !product_image ||
-            !status
-        ) {
-            return NextResponse.json(
-                {
-                    message:
-                        "Missing required fields: key, name, stock, container_type, uom, volume, flavor, default_labeled_price, default_cost, product_image, or status",
-                },
-                { status: 400 }
-            );
-        }
+		// Validation
+		if (
+			!key ||
+			!name ||
+			!stock ||
+			!container_type ||
+			!uom ||
+			!volume ||
+			!flavour ||
+			!default_labeled_price ||
+			!default_cost ||
+			!product_image ||
+			!status
+		) {
+			return NextResponse.json(
+				{
+					message:
+						"Missing required fields: key, name, stock, container_type, uom, volume, flavor, default_labeled_price, default_cost, product_image, or status",
+				},
+				{ status: 400 }
+			);
+		}
+		//convert stock, volume, default_labeled_price, default_cost to float
+		stock = parseFloat(stock);
+		volume = parseFloat(volume);
+		default_labeled_price = parseFloat(default_labeled_price);
+		default_cost = parseFloat(default_cost);
+		uom = parseFloat(uom);
 
-        // Update product
-        const product = await prisma.product.update({
-            where: { key : productKey||"" },
-            data: {
-                barcode,
-                name,
-                stock,
-                container_type,
-                uom,
-                volume,
-                flavour,
-                default_labeled_price,
-                default_cost,
-                product_image,
-                status,
-            },
-        });
+		//convert barcode, key, name, container_type, uom, flavour, product_image, status to string
+		barcode = barcode.toString();
+		key = key.toString();
+		name = name.toString();
+		container_type = container_type.toString();
+		flavour = flavour.toString();
+		product_image = product_image.toString();
+		status = status.toString();
 
-        return NextResponse.json({ message: "Product updated", product });
-    } catch (e: any) {
-        return NextResponse.json(
-            { message: "Error updating product " + e.message, error: e },
-            { status: 500 }
-        );
-    }
+		// Update product
+		const product = await prisma.product.update({
+			where: { key: productKey || "" },
+			data: {
+				barcode,
+				name,
+				stock,
+				container_type,
+				uom,
+				volume,
+				flavour,
+				default_labeled_price,
+				default_cost,
+				product_image,
+				status,
+			},
+		});
+
+		return NextResponse.json({ message: "Product updated", product });
+	} catch (e: any) {
+		return NextResponse.json(
+			{ message: "Error updating product " + e.message, error: e },
+			{ status: 500 }
+		);
+	}
 }
 
 export async function DELETE(req: NextRequest) {
-    const userHeader = req.headers.get("user");
-    const productKey = req.nextUrl.searchParams.get("key");
-    const user: UserType | null = userHeader ? JSON.parse(userHeader) : null;
+	const userHeader = req.headers.get("user");
+	const productKey = req.nextUrl.searchParams.get("key");
+	const user: UserType | null = userHeader ? JSON.parse(userHeader) : null;
 
-    if (!user) {
-        return NextResponse.json({ message: "User not found" }, { status: 404 });
-    }
+	if (!user) {
+		return NextResponse.json({ message: "User not found" }, { status: 404 });
+	}
 
-    if (user.role !== "admin") {
-        return NextResponse.json(
-            { message: "Unauthorized: Only admin can delete products" },
-            { status: 401 }
-        );
-    }
+	if (user.role !== "admin") {
+		return NextResponse.json(
+			{ message: "Unauthorized: Only admin can delete products" },
+			{ status: 401 }
+		);
+	}
 
-    try {
-        // Delete product
-        const product = await prisma.product.delete({
-            where: { key : productKey||"" },
-        });
+	try {
+		// Delete product
+		const product = await prisma.product.delete({
+			where: { key: productKey || "" },
+		});
 
-        return NextResponse.json({ message: "Product deleted", product });
-    } catch (e: any) {
-        return NextResponse.json(
-            { message: "Error deleting product", error: e },
-            { status: 500 }
-        );
-    }
+		return NextResponse.json({ message: "Product deleted", product });
+	} catch (e: any) {
+		return NextResponse.json(
+			{ message: "Error deleting product", error: e },
+			{ status: 500 }
+		);
+	}
 }
