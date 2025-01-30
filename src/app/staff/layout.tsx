466dbc6176a85762f01cd364a975518abed78606
Malith Dilshan <it21180934@my.sliit.lk>
2025-01-31 01:07:54 +0530

    staff panel loading animation ✨

--- src/app/staff/layout.tsx
+++ src/app/staff/layout.tsx
@@ -1,11 +1,42 @@
+"use client"
+import Loading from "@/components/loading";
+import axios from "axios";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function StaffLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-    
+    const [status, setStatus] = useState("loading");//success, error
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if(!token){
+            setStatus("error");
+        }else{
+            axios.get("/api/auth/user", {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }).then((res) => {
+                if(res.data.message === "User Found"){
+                    //chekck if user is staff manager or admin
+                    if(res.data.user.role === "staff" || res.data.user.role === "manager" || res.data.user.role === "admin"){
+                        setStatus("success");
+                    }else{
+                        setStatus("error");
+                    }
+                }else{
+                    setStatus("error");
+                }
+            }).catch(() => {
+                setStatus("error");
+            }
+            )
+        }
+    },[])
+
 
     return(
         <div className="w-full h-screen max-h-screen">
@@ -13,7 +44,10 @@ export default function StaffLayout({
                 <Image src="/favicon.png" alt="Adikaram Enterprises Logo" className="bg-white rounded-full p-1 absolute m-1 left-1" width={50} height={50} />
                 <h1 className="text-2xl font-bold  mx-auto">Adikaram Enterprises</h1>
             </div>
-            {children}
+            {status=="success"
+            &&children}
+            {status=="loading"&&<Loading/>}
+            {status=="error"&&<div className="flex items-center justify-center h-full">You are not authorized to view this page</div>}
         </div>
     )
 }
