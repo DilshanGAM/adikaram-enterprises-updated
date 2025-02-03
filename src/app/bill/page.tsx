dcc6ed856fb297d003454bfd5ed7e944fd4bf387
Malith Dilshan <it21180934@my.sliit.lk>
2025-02-04 00:09:45 +0530

    commented error🫡

--- src/app/bill/page.tsx
+++ src/app/bill/page.tsx
@@ -3,9 +3,9 @@
 import { useState } from "react";
 
 export default function BillPage() {
-    const [status, setStatus] = useState("loading");//loading, success, error
-    const [data, setData] = useState<any>(null);
-    const token = localStorage.getItem("token");
+    // const [status, setStatus] = useState("loading");//loading, success, error
+    // const [data, setData] = useState<any>(null);
+    // const token = localStorage.getItem("token");
     return (
         <div className="w-full">
             <h1>Bill Page</h1>
